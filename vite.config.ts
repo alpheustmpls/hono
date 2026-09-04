66e4b21f8c2ee692c88c07a03db172c491f586ef
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-09-04 08:21:26 +0800

    feat: use vite built-in tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import * as Path from "node:path";
 import { srvkit } from "@srvkit/vite/plugin";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+    resolve: {
+        tsconfigPaths: true,
+    },
     plugins: [
         process.env.DEV_HTTPS === "1" &&
             mkcert({
                 savePath: Path.resolve(process.cwd(), "node_modules", ".cert"),
             }),
-        tsconfigPaths(),
         srvkit(),
     ],
 });
